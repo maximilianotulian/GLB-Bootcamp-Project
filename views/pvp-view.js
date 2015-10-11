7af5755e19fa1bfc7b50a3b0adc24c9d917b13f3
maximilianotulian <maxtulian93@hotmail.com>
2015-10-11 18:26:47 -0300

    [Maximiliano Tulian] - pvp-view

--- views/pvp-view.js
+++ views/pvp-view.js
@@ -1,12 +1,47 @@
+// LIBS
 var React = require('react');
 
+// SERVICES
+var characterApi = require('../services/character-api');
+
+// COMPONENTS
+var Table = require('../components/table');
+
 var PvpView = React.createClass({
+
+    getInitialState: function () {
+        characterApi.getLeaderBoards(this.updateState);
+        return {
+            pvp: []
+        }
+    },
+
     render: function () {
         return (
             <div>
-                PVP
+                <Table {...this.getTableProps()}/>
             </div>
         )
+    },
+
+    getTableProps: function () {
+        return {
+            rows: this.getRows(),
+            rowsContent: this.getRowsContent()
+        }
+    },
+
+    getRows: function () {
+        return ['ranking', 'name', 'realmName','seasonLosses','seasonWins']
+    },
+
+    getRowsContent: function () {
+        return this.state.pvp;
+    },
+
+    updateState: function (result) {
+        console.log(result);
+        this.setState({pvp: result.rows});
     }
 });
 
