c5e4d56e17a14c44cad0abb94766d13003808f5f
Maximiliano Tulian <maxtulian93@hotmail.com>
2015-10-22 21:09:10 -0300

    [Maximiliano Tulian] - update button tests

--- components/_tests_/button-test.js
+++ components/_tests_/button-test.js
@@ -1,22 +1,28 @@
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 var Button = require('../button');
-
 var button;
-describe('button', function () {
+var buttonWithChildren;
+
+describe('button tests', function () {
 
     describe('when the button have default props', function (){
         button = TestUtils.renderIntoDocument(<Button />);
-        it('', function () {
+
+        console.log(button.props);
+        it('expect statements to be defined ', function () {
             expect(button.getClass()).to.equal('custom-button');
-            expect(button.props.children).to.equal('undefined');
+        });
+        it('expect statement to be undefined', function () {
+            console.log(button.props.children);
+            expect(button.props.children).to.equal(undefined);
         });
     });
 
     describe('when the button have childrems', function () {
-        button = TestUtils.renderIntoDocument(<Button>lalala</Button>);
-        it('', function() {
-            expect(button.props.children).to.equal('lalala');
+        buttonWithChildren = TestUtils.renderIntoDocument(<Button>lalala</Button>);
+        it('expect statements to be defined', function() {
+            expect(buttonWithChildren.props.children).to.equal('lalala');
         });
     });
 });
